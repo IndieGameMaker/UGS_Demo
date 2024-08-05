fa1222d98a6342074fb7141e759dc5702bd69c43
Lee JaeHyun <myleje@gmail.com>
2024-08-05 14:59:01 +0900

    .

--- Assets/02_Scripts/AuthManager.cs
+++ Assets/02_Scripts/AuthManager.cs
@@ -1,4 +1,5 @@
 using TMPro;
+using Unity.Services.Core;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,5 +8,9 @@ public class AuthManager : MonoBehaviour
     [SerializeField] private Button loginButton;
     [SerializeField] private TMP_Text messageText;
 
-
+    private async void Awake()
+    {
+        // UGS 초기화
+        await UnityServices.InitializeAsync();
+    }
 }
