3a6e334c1341aa0c3199f4175ab0f25a75aef684
Lee JaeHyun <myleje@gmail.com>
2024-08-06 15:38:11 +0900

    .

--- Assets/02_Scripts/RCManager.cs
+++ Assets/02_Scripts/RCManager.cs
@@ -1,10 +1,16 @@
+#pragma warning disable IDE0051, UNT0006
+
 using System.Threading.Tasks;
 using Unity.Services.Authentication;
 using Unity.Services.Core;
 using UnityEngine;
+using Unity.Services.RemoteConfig;
 
 public class RCManager : MonoBehaviour
 {
+    public float moveSpeed;
+    public float playerScale;
+
     private async Task Awake()
     {
         // 유니티 서비스 초기화
@@ -17,4 +23,7 @@ public class RCManager : MonoBehaviour
         // 익명 로그인 처리
         await AuthenticationService.Instance.SignInAnonymouslyAsync();
     }
+
+    // 데이터 로드를 위한 구조체 선언
+    public struct
 }
