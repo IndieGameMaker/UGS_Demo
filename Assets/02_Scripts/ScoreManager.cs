7a9ee1c622ce0dd7e8ed211c7d6957e4f9ea7294
Lee JaeHyun <myleje@gmail.com>
2024-08-06 16:28:17 +0900

    .

---
+++ Assets/02_Scripts/ScoreManager.cs
@@ -0,0 +1,25 @@
+using System.Threading.Tasks;
+using TMPro;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField] private Button saveButton;
+    [SerializeField] private TMP_InputField scoreIf;
+
+    private async Task Awake()
+    {
+        // 유니티 서비스 초기화
+        await UnityServices.InitializeAsync();
+        // 익명 로그인 이벤트
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log($"로그인 완료 : {AuthenticationService.Instance.PlayerId}");
+        };
+        // 익명 로그인 처리
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+}
